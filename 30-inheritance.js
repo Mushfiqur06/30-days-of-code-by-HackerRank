e99913072bd7310db92460f585bb23ed34d9d5d8
Mushfiqur Rahman <mushfiqur06@gmail.com>
2018-04-03 02:33:40 +0600

    30-inheritance.js problem solve

---
+++ 30-inheritance.js
@@ -0,0 +1,24 @@
+
+calculate() {
+    var sum = this.scores.reduce(function(sum, num) 
+    { 
+        return sum + num;
+    });
+    
+    var score = Math.ceil(sum / this.scores.length);
+    var letter = "";
+    if (90 <= score && score <= 100)
+        letter = "O";
+    else if (80 <= score && score < 90)
+        letter = "E";
+    else if (70 <= score && score < 80)
+        letter = "A";
+    else if (55 <= score && score < 70)
+        letter = "P";
+    else if (40 <= score && score < 55)
+        letter = "D";
+    else if (score < 40)
+        letter = "T"
+    
+    return letter;
+}
